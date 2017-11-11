2d79d64e612d185808b19aa3b98b954933ecee7b
Duggie <duggie@continual.ly>
2017-11-11 09:46:15 +0000

    refactored to slim down Mail class

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # mail
 A simple email sending class
+
+## Dependencies
+
+* PHP7.0+ (would be lower but I've used native type-hinting)
+* PHP Composer
+
+## Build
+
+Run `composer install`.
+
+Copy `.env.example` to `.env` and update the settings contained within.
+
+## Usage
+
+I recommend using autoloading so things "Just Work".
+
+```
+use App\Mail;
+
+// Here is an example for an HTML email.
+
+$mail = new Mail;
+
+$mail->addTo('jane.doe@example.com', 'Jane Doe');
+$mail->setSubject('This is the subject line');
+$mail->setHtmlMessage("<h1>Test Message</h1>
+<p>Hello world!</p>");
+
+try {
+    $mail->send();
+} catch (Exception $e) {
+    printf("[code %d] Unable to send your email to: %s (%s) - %s",
+        $e->getCode(),
+        json_encode($mail->getToAddresses()),
+        $mail->Subject,
+        $e->getMessage()
+    );
+}
+```
+
+An example script can be found [here](script.php).
+
+## Tests
+
+You can run PHPUnit tests: `vendor/bin/phpunit`
+
+A "Lines of code" report is [here](tests/coverage/lines_of_coverage.txt)
+
+## This is CRAP
+
+Coverage test results, including a CRAP index rating, can be found [here](tests/coverage/)
+
+## Built with...
+
+* PHPStorm
+* [phpcs](https://github.com/squizlabs/PHP_CodeSniffer) - PHPCodeSniffer
+* [phploc](https://github.com/sebastianbergmann/phploc) - lines of code report generator
+* [FIG-PSR2](https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md) coding standard 
+* [Composer](https://getcomposer.org/) - PHP package dependency manager
--- src/Mail.php
+++ src/Mail.php
@@ -9,19 +9,21 @@
 
 namespace App;
 
-use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
+use App\Traits\MailTrait;
 
 final class Mail extends PHPMailer
 {
+    use MailTrait;
+
     /**
- * @var int SMTP debug flag: 0 = off
-*/
+     * @var int SMTP debug flag: 0 = off
+     */
     const SMTP_DEBUG = 0;
 
     /**
- * @var string default driver for sending emails
-*/
+     * @var string default driver for sending emails
+     */
     const DRIVER = 'mail';
 
     /**
@@ -116,98 +118,5 @@ final class Mail extends PHPMailer
         $this->preSendValidation();
 
         return parent::send();
-    }
-
-    /**
-     * Set the preferred mail driver.
-     */
-    private function setDriver()
-    {
-        if (! $driver = getenv('MAIL_DRIVER')) {
-            $driver = self::DRIVER;
-        }
-
-        $this->driver = $driver;
-    }
-
-    /**
-     * Set how the mail should be sent.
-     */
-    private function setTransport()
-    {
-        $method = sprintf('setTransportTo%s', ucfirst($this->driver));
-
-        if (! method_exists(self::class, $method)) {
-            throw new Exception('Unsupported transport method');
-        }
-
-        $this->{$method}();
-    }
-
-    /**
-     * Set mail sending method to use mail().
-     */
-    private function setTransportToMail()
-    {
-        $this->isMail();
-    }
-
-    /**
-     * Set mail sending method to use SMTP.
-     */
-    private function setTransportToSmtp()
-    {
-        $this->configureSMTP();
-    }
-
-    /**
-     * Set mail sending method to use Sendmail.
-     */
-    private function setTransportToSendmail()
-    {
-        $this->isSendmail();
-    }
-
-    /**
-     * Configure settings for sending via SMTP.
-     */
-    private function configureSMTP()
-    {
-        if (! $debug = getenv('MAIL_SMTP_DEBUG')) {
-            $debug = self::SMTP_DEBUG;
-        }
-
-        $this->isSMTP();
-        $this->SMTPAuth = true;
-        $this->SMTPDebug = $debug;
-        $this->Host = getenv('MAIL_SMTP_HOST');
-        $this->Port = getenv('MAIL_SMTP_PORT');
-        $this->Username = getenv('MAIL_SMTP_USERNAME');
-        $this->Password = getenv('MAIL_SMTP_PASSWORD');
-    }
-
-    /**
-     * Set up the default "From" address.
-     */
-    private function initialiseFromAddress()
-    {
-        $this->setFrom(getenv('MAIL_FROM_EMAIL'), getenv('MAIL_FROM_NAME'));
-        $this->setReplyTo($this->From, $this->FromName);
-    }
-
-    /**
-     * Ensure the email is well-formed before sending it.
-     *
-     * @throws Exception
-     */
-    private function preSendValidation()
-    {
-        if (! $this->From) {
-            throw new Exception('No From address');
-        }
-
-        if (! $this->Subject) {
-            throw new Exception('No Subject line');
-        }
     }
 }
